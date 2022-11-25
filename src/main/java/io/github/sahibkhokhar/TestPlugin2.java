6051403052ac8d878ff68db5fea24defa654687d
Sahib Khokhar <skhokhar2003@gmail.com>
2022-11-25 04:20:28 -0500

    added CommandBruh.java

--- src/main/java/io/github/sahibkhokhar/TestPlugin2.java
+++ src/main/java/io/github/sahibkhokhar/TestPlugin2.java
@@ -1,2 +1,20 @@
-package io.github.sahibkhokhar;public class TestPlugin2 {
+package io.github.sahibkhokhar;
+
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TestPlugin2 extends JavaPlugin implements Listener {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
+    }
 }
